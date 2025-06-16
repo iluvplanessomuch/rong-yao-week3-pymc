2c4ab68f600d749fc7fb4bfa771bc18f48f64420
iluvplanessomuch <lee_rong_yao@students.edu.sg>
2025-06-16 04:18:42 +0000

    Jun 16, 2025, 4:18 AM

--- Day1Folder/day1.py
+++ Day1Folder/day1.py
@@ -1,34 +1,83 @@
 # Write all your codes for Day 1 here.
 # COMMENT out the previous task before going on to the next task
 print("hello from day1")
+def intro(newfriend):
+    print("i am rong yao")
+    print("i am 13 and i am from swiss cottage")
+    print("i love planes")
+    print("nice to meet u, " + newfriend)
+intro("mary")
+intro("peter")
+intro("johnny")
+intro("austrian painter")
+intro("jeff")
+intro("timmy")
+intro("obama")
+intro("harry pottah")
+intro("bomb man")
+intro("urainium 235")
+intro("oxygen")
+intro("technitium")
+
+
 
 ########################################################################
 # Task 1:
+# print("i am rong yao")
+# print("i am 13 years old")
+# print('swiss cottage')
+# poo = 10
+# print(poo ++ 112401982409172975)
+
+
+
 
 
 
 ########################################################################
 # Task 2:
+# print("i would invest in stocks and gold")
 
 
 
 ########################################################################
-# Task 3:
-
-
+# # Task 3:
+# animal1 = "tiger"
+# animal2 = "spinosaurus"
+# animal3 = "human"
+# print (animal1)
+# print (animal2)
+# print (animal3)
+# print (animal1 + animal2)
+# print (animal1 * 10)
 
 ########################################################################
 # Task 4:
-
+# variable1 = 10
+# variable2 = 112
+# print (variable1 + variable2)
 
 
 ########################################################################
 # Task 5:
-
+# name = "poop"
+# message = "gotta go toilet"
+# print (name + " says " + message)
+name = "rong yao"
+food = "grass"
+moneh = 101010101
+print ("i am " + name)
+print ("i like " + food + str(moneh))
+print ("i luv moneh " + str(moneh))
 
 
 ########################################################################
 # Task 6:
+# def greetings( ) : 
+#     print("poopyface")
+#     print("goofyahh")
+# greetings( )
+    
 
 
 
@@ -36,6 +85,30 @@ print("hello from day1")
 # Task 7:
 
 
+def addition(number1, number2):
+
+
+
+    print(number1 + number2)
+addition(10,124)
+def subtract(number1, number2):
+
+
+
+    print(number1 - number2)
+subtract(35135,135)
+def divide(number1, number2):
+
+
+
+    print(number1 / number2)
+divide(124,3)
+def multiply(number1, number2):
+
+
+
+    print(number1 * number2)
+multiply(12,10)
 
 ########################################################################
 # Additional exercises:
--- Day1Folder/minecraft_day1.py
+++ Day1Folder/minecraft_day1.py
@@ -3,4 +3,79 @@
 ################# Function Section #############################
 
 
+
+def on_chat():
+    agent.teleport_to_player()
+    
+player.on_chat("fart", on_chat)
+
+
+def turnleft(steps):
+    agent.turn(LEFT)
+    
+player.on_chat("LEFT", turnleft)
+def turnright():
+        agent.turn(RIGHT)
+player.on_chat("RIGHT", turnright)
+
+
+def movef(steps):
+    agent.move(FORWARD,steps)
+player.on_chat("fore", movef)
+def moveb(steps):
+        agent.move(BACK,steps)
+player.on_chat("back", moveb)
+def moveu(steps):
+    agent.move(UP,steps)
+player.on_chat("up", moveu)
+def moved(steps):
+    agent.move(DOWN,steps)
+player.on_chat("down", moved)
+
+def on_chat2():
+    agent.move(FORWARD, 4)
+    agent.turn(LEFT)
+    agent.move(FORWARD, 4)
+    agent.turn(RIGHT)
+    agent.move(FORWARD, 3)
+    agent.move(UP, 1)
+    agent.move(FORWARD, 1)
+    agent.move(UP, 1)
+    agent.move(FORWARD, 1)
+    agent.move(UP, 1)
+    agent.move(FORWARD, 1)
+    agent.move(FORWARD, 1)
+    agent.move(FORWARD, 1)
+    agent.move(DOWN, 1)
+    agent.move(FORWARD, 1)
+    agent.move(DOWN, 1)
+    agent.move(FORWARD, 1)
+    agent.move(DOWN, 1)
+def obby2():
+    agent.turn(TurnDirection.LEFT)
+    agent.turn(TurnDirection.LEFT)
+    agent.move(UP, 1)
+    agent.move(FORWARD, 1)
+    agent.move(UP, 1)
+    agent.move(FORWARD, 1)
+    agent.move(UP, 1)
+    agent.move(FORWARD, 1)
+    agent.move(FORWARD, 1)
+    agent.move(FORWARD, 1)
+    agent.move(DOWN, 1)
+    agent.move(FORWARD, 1)
+    agent.move(DOWN, 1)
+    agent.move(FORWARD, 1)
+    agent.move(DOWN, 1)
+    agent.move(FORWARD, 98765)
+    agent.turn(TurnDirection.LEFT)
+    agent.move(FORWARD, 141251235135)
+    agent.turn(RIGHT)
+    agent.move(FORWARD, 4)
+
+
+
+player.on_chat("obby2", obby2)
+
+
 ################## On Chat Commands Section #####################
