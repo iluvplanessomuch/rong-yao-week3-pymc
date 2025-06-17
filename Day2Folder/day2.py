d7dd58d0fd9cf1185ce431a6cf7dca3fdcf51fef
iluvplanessomuch <lee_rong_yao@students.edu.sg>
2025-06-17 02:08:33 +0000

    Jun 17, 2025, 2:08 AM

--- Day2Folder/day2.py
+++ Day2Folder/day2.py
@@ -13,7 +13,7 @@ print("hello from day2")
 # Task 2:
 for counter in range(5):
     print(counter+1)    
-   
+
 for letter in "JAYDENLINGCHANGHAO":
     print( "gimme a " + letter)
 print("whos the best?")
