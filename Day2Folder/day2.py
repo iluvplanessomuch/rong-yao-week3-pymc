4962a796d403cadc878f56c25ae02a713e83d0e0
iluvplanessomuch <lee_rong_yao@students.edu.sg>
2025-06-17 02:07:33 +0000

    Jun 17, 2025, 2:07 AM

--- Day2Folder/day2.py
+++ Day2Folder/day2.py
@@ -16,7 +16,7 @@ for counter in range(5):
    
 for letter in "JAYDENLINGCHANGHAO":
     print( "gimme a " + letter)
-print
+print("whos the best?")
 
 ########################################################################
 # Task 3:
