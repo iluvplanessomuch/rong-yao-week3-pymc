814d0edc7f7cf3b7544d3de47def5873bae119fa
iluvplanessomuch <lee_rong_yao@students.edu.sg>
2025-06-19 01:14:14 +0000

    Jun 19, 2025, 1:14 AM

--- Day2Folder/minecraft_day2.py
+++ Day2Folder/minecraft_day2.py
@@ -1,2 +1,122 @@
 # AT THE END OF LESSON, COPY YOUR CODE FROM MINECRAFT HERE.
 # THIS IS SO THAT YOU HAVE A RECORD OF YOUR CODE FROM MINECRAFT CODE BUILDER
+
+
+
+def on_chat():
+    agent.teleport_to_player()
+    
+player.on_chat("fart", on_chat)
+
+
+def turnleft(steps):
+    agent.turn(LEFT)
+    
+player.on_chat("LEFT", turnleft)
+def turnright():
+        agent.turn(RIGHT)
+player.on_chat("RIGHT", turnright)
+
+
+def movef(steps):
+    agent.move(FORWARD,steps)
+player.on_chat("fore", movef)
+def moveb(steps):
+        agent.move(BACK,steps)
+player.on_chat("back", moveb)
+def moveu(steps):
+    agent.move(UP,steps)
+player.on_chat("up", moveu)
+def moved(steps):
+    agent.move(DOWN,steps)
+player.on_chat("down", moved)
+
+def on_chat2():
+    agent.move(FORWARD, 4)
+    agent.turn(LEFT)
+    agent.move(FORWARD, 4)
+    agent.turn(RIGHT)
+    agent.move(FORWARD, 3)
+    agent.move(UP, 1)
+    agent.move(FORWARD, 1)
+    agent.move(UP, 1)
+    agent.move(FORWARD, 1)
+    agent.move(UP, 1)
+    agent.move(FORWARD, 1)
+    agent.move(FORWARD, 1)
+    agent.move(FORWARD, 1)
+    agent.move(DOWN, 1)
+    agent.move(FORWARD, 1)
+    agent.move(DOWN, 1)
+    agent.move(FORWARD, 1)
+    agent.move(DOWN, 1)
+def obby2():
+    agent.turn(TurnDirection.LEFT)
+    agent.turn(TurnDirection.LEFT)
+    agent.move(UP, 1)
+    agent.move(FORWARD, 1)
+    agent.move(UP, 1)
+    agent.move(FORWARD, 1)
+    agent.move(UP, 1)
+    agent.move(FORWARD, 1)
+    agent.move(FORWARD, 1)
+    agent.move(FORWARD, 1)
+    agent.move(DOWN, 1)
+    agent.move(FORWARD, 1)
+    agent.move(DOWN, 1)
+    agent.move(FORWARD, 1)
+    agent.move(DOWN, 1)
+    agent.move(FORWARD, 98765)
+    agent.turn(TurnDirection.LEFT)
+    agent.move(FORWARD, 141251235135)
+    agent.turn(RIGHT)
+    agent.move(FORWARD, 4)
+
+
+
+player.on_chat("obby2", obby2)
+
+def on_chat3():
+    mobs.apply_effect(HEALTH_BOOST, mobs.target(NEAREST_PLAYER), 10, 1)
+player.on_chat("food", on_chat3)
+
+
+def choppy(height):
+    agent.destroy(FORWARD)
+    agent.move(FORWARD, 1)
+    for count in range(height):
+        agent.destroy(UP)
+        agent.move(UP, 1)
+player.on_chat("chop", choppy)
+
+def cobbly(length):
+    for count in range(length):
+
+        agent.destroy(LEFT)
+        agent.destroy(RIGHT)
+        agent.destroy(DOWN)
+        agent.move(FORWARD, 1)
+
+player.on_chat("cobble", cobbly)
+def on_chat4(turnaround):
+    agent.turn(TurnDirection.LEFT)
+    agent.turn(TurnDirection.LEFT)
+player.on_chat("turnaround", on_chat4)
+
+def bobthebuilder():
+    for count in range(30):
+        agent.place(UP)
+        agent.place(DOWN)
+        
+        agent.move(BACK, 1)
+        agent.place(FORWARD)
+
+player.on_chat("bbb", bobthebuilder)
+
+
+
+
+
+
+
+
--- Day3Folder/day3.py
+++ Day3Folder/day3.py
@@ -1,35 +1,76 @@
 # Write all your codes for Day 3 here.
 # COMMENT out the previous task before going on to the next task
-print("hello from day3")
+# print("hello from day3")
+
+# name = input("what is your name?")
+# print("Oh! HELLO " + name)
+# for namae in name:
+#     print("gimme a " + namae)
+# print("whos the best? " + name + "!")
+
+# age = input("how old are you? ")
+# meage = int(age)+2
+# print("HEYYY I AM "+ str(meage) + " so cool!")
+
+
+
+
+
+
 
 ########################################################################
 # Task 1:
+# name = input("what is your name?")
+# title = input("what is your title?")
+# command = input("what is your command?")
+# print(title +" "+ name + " "+ "COMMANDS THE PEASANTS TO " + command)
 
 
 
 ########################################################################
 # Task 2:
-
+# name = input("hello? what is your name? ")
+# buy = input("hello " + name + " what would you like to buy? ")
+# numba = input("and how many "+ buy + " you you want to buy?")
+# print( name + " wants to buy "+ str(numba) +" amount of "+ buy )
 
 
 ########################################################################
 # Task 3:
-
-
+# num1 = input("gimme a numba: ")
+# num2 = input("gimme anotha numba: ")
+# results = int(num1)+int(num2)
+# print("da ansa is "+ str(results))
 
 ########################################################################
 # Task 4:
-
-
+# wahtdeywan = input("what do you want to buy? ")
+# howmany = input("how much do you want to buy? ")
+# cost = int(howmany) * 0.50 
+# print(str(howmany) + " amount of " + wahtdeywan + " will cost you "+ str(cost))
 
 ########################################################################
 # Task 5:
-
-
-
+# print("is it okay?")
+# big = input("man? ")
+# small = input("woman? ")
+# num1 = int(big)
+# num2 = int(small)
+# if num1 < num2 :
+#     print('YES!')
+# else :
+#     print('NAHHHHHH')
 ########################################################################
 # Task 6:
+realpass = 'abcdefg'
+for count in range(3):
+    guess = input('what is the password ')
 
+    if realpass == guess :
+        print('access granted')
+    else :
+        print('access denied')
+print('you stupid!')
 
 
 ########################################################################
